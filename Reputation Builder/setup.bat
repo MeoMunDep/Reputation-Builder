59473e027c439a0b46408ec09a400c03f047b153
BlackCat <184516521+MeoMunDep@users.noreply.github.com>
2025-04-11 18:08:34 +0700

    Update setup.bat

--- Reputation Builder/setup.bat
+++ Reputation Builder/setup.bat
@@ -6,8 +6,7 @@ cd %~dp0
 
 echo Checking configuration files...
 
-
-(for %%F in (datas.txt wallets.txt proxies.txt) do (
+(for %%F in (datas.txt privateKeys.txt proxies.txt) do (
     if not exist %%F (
         type nul > %%F
         echo Created %%F
@@ -20,11 +19,11 @@ echo Checking dependencies...
 if exist "..\node_modules" (
     echo Using node_modules from parent directory...
     cd ..
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent solc web3 ethers bs58
     cd %~dp0
 ) else (
     echo Installing dependencies in current directory...
-    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent 
+    CALL npm install user-agents axios colors https-proxy-agent socks-proxy-agent solc web3 ethers bs58
 )
 echo Dependencies installation completed!
 
